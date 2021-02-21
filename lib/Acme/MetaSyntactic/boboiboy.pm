56ae97142bdf7c1bddff8254a7ec96614733600b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-21 07:25:15 +0700

    Initial commit

---
+++ lib/Acme/MetaSyntactic/boboiboy.pm
@@ -0,0 +1,40 @@
+package Acme::MetaSyntactic::boboiboy;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use parent qw(Acme::MetaSyntactic::MultiList);
+__PACKAGE__->init;
+
+1;
+# ABSTRACT: The BoboiBoy theme
+
+=head1 SYNOPSIS
+
+ % perl -MAcme::MetaSyntactic=boboiboy -le 'print metaname'
+ fang
+
+ % metasyn boboiboy | shuf | head -n2
+ yaya
+ gopal
+
+
+=head1 SEE ALSO
+
+L<https://en.wikipedia.org/wiki/BoBoiBoy>,
+L<https://boboiboy.fandom.com/wiki/BoBoiBoy_Wiki>
+
+L<Acme::MetaSyntactic>
+
+=cut
+
+__DATA__
+# default
+:all
+# names tmp_all
+boboiboy yaya fang ying gopal
+tokaba papazola kokoci tarung kaizo maskmana motobot ramenman
+adudu probe ejojo borara retakka
+amato totoitoy atata bagogo iwan sasha azroy popo razak munch conon tauke timi melissa melody alebi biscuit kpkt yim wahid
